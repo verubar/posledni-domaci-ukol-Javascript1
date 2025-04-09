962eaa6b384a03de7338e77642310c6b16675455
Veronika Gawlasová <v.gawlasova@gmail.com>
2025-04-09 16:17:10 +0200

    do třetice všeho dobrého

--- script.js
+++ script.js
@@ -7,85 +7,96 @@ const numbers = [
   ];
 
 // Vypište do stránky všechna čísla.
-// numbers.forEach((hodnota) => {
-//      document.body.innerHTML += `${hodnota} <br>`}
-// );
+const h1 = document.querySelector('.uloha1');
+numbers.forEach((hodnota) => {
+    h1.innerHTML += ` ${hodnota}, `});
 
 // Vypište do stránky druhé mocniny všech čísel.
-// numbers.forEach((hodnota) => {
-//     document.body.innerHTML += `${Math.pow(hodnota, 2)} <br>`}
-// );
+const h2 = document.querySelector('.uloha2');
+numbers.forEach((hodnota) => {
+    h2.innerHTML += ` ${Math.pow(hodnota, 2)}, `}
+);
 
 // Vypište do stránky pouze záporná čísla.
-// numbers.forEach((hodnota) => {
-//     if (hodnota < 0) {
-//     document.body.innerHTML += `${hodnota} <br>`}
-// });
+const h3 = document.querySelector('.uloha3');
+numbers.forEach((hodnota) => {
+    if (hodnota < 0) {
+    h3.innerHTML += ` ${hodnota}, `}
+});
 
 // Vypište do stránky absolutní hodnotu všech čísel.
-// numbers.forEach((hodnota) => {
-//     document.body.innerHTML += `${Math.abs(hodnota)} <br>`}
-// );
+const h4 = document.querySelector('.uloha4');
+numbers.forEach((hodnota) => {
+    h4.innerHTML += ` ${Math.abs(hodnota)}, `}
+);
 
 // Vypište do stránky pouze sudá čísla.
-// numbers.forEach((hodnota) => {
-//     if (hodnota % 2 === 0) {
-//         document.body.innerHTML += `${hodnota} <br>`
-//       }
-// });
+const h5 = document.querySelector('.uloha5');
+numbers.forEach((hodnota) => {
+    if (hodnota % 2 === 0) {
+        h5.innerHTML += ` ${hodnota}, `
+      }
+});
 
 // Vypište do stránky pouze ta čísla, jejíchž absolutní hodnota je dělitelná třemi.
-// numbers.forEach((hodnota) => {
-//     if (((Math.abs(hodnota)) % 3) ===  0) {
-//          document.body.innerHTML += `${hodnota} <br>`
-//     }
-// });
+const h6 = document.querySelector('.uloha6');
+numbers.forEach((hodnota) => {
+    if ((((hodnota)) % 3) ===  0) {
+         h6.innerHTML += ` ${Math.abs(hodnota)}, `
+    }
+});
 
 // Vypište do stránky jak daleko je každé číslo v seznamu od čísla 5.
-// const ciloveCislo = 5;
-// numbers.forEach((hodnota) => {
-//     const vzdalenost = Math.abs(hodnota - ciloveCislo);
-//     document.body.innerHTML += `${vzdalenost} <br>`;
-//   });
+const h7 = document.querySelector('.uloha7');
+const ciloveCislo = 5;
+numbers.forEach((hodnota) => {
+    const vzdalenost = Math.abs(hodnota - ciloveCislo);
+    h7.innerHTML += ` ${vzdalenost}, `;
+  });
 
 // Vypište do stránky druhé mocnicny vzdáleností všech čísel od čísla 5.
-// const ciloveCislo = 5;
-// numbers.forEach((hodnota) => {
-//     const vzdalenost = Math.abs(hodnota - ciloveCislo);
-//     document.body.innerHTML += `${Math.pow(vzdalenost, 2)} <br>`;
-//   });
+const h8 = document.querySelector('.uloha8');
+const ciloveCislo2 = 5;
+numbers.forEach((hodnota) => {
+    const vzdalenost = Math.abs(hodnota - ciloveCislo2);
+    h8.innerHTML += ` ${Math.pow(vzdalenost, 2)}, `;
+  });
 
 // Spočítejte, kolik je v seznamu záporných čísel.
-// let vysledek = 0;
-// numbers.forEach((hodnota) => {
-//     if (hodnota < 0) {
-//         vysledek = vysledek + 1;
-//     }
-// });
-// document.body.innerHTML += `${vysledek}`;
+const h9 = document.querySelector('.uloha9');
+let vysledek = 0;
+numbers.forEach((hodnota) => {
+    if (hodnota < 0) {
+        vysledek = vysledek + 1;
+    }
+});
+h9.innerHTML += ` ${vysledek}`;
 
-// Spočítejte součet všech čísel v poli. ***
-// let soucet = 0;
-// numbers.forEach((hodnota) => {
-//     soucet = soucet + hodnota; // můžeme zapsat jako soucet += hodnota; 
-// });
-// document.body.innerHTML += `${soucet}`;
+// Spočítejte součet všech čísel v poli.
+const h10 = document.querySelector('.uloha10');
+let soucet = 0;
+numbers.forEach((hodnota) => {
+    soucet = soucet + hodnota; // můžeme zapsat jako soucet += hodnota; 
+});
+h10.innerHTML += ` ${soucet}`;
 
 // Spočítejte průměr všech čísel v poli.
-// let soucet = 0;
-// let predsoucet = 0;
-// let vysledek = 0;
-// numbers.forEach((hodnota) => {
-//     predsoucet = soucet + hodnota;
-//     vysledek = predsoucet / numbers.length
-// });
-// document.body.innerHTML += `${vysledek}`;
+const h11 = document.querySelector('.uloha11');
+let soucet2 = 0;
+let predsoucet = 0;
+let vysledek2 = 0;
+numbers.forEach((hodnota) => {
+    predsoucet = soucet2 + hodnota;
+    vysledek2 = predsoucet / numbers.length
+});
+h11.innerHTML += ` ${vysledek2}`;
 
 // Spočítejte součet všech kladných čísel v poli.
-// let soucet = 0;
-// numbers.forEach((hodnota) => {
-//     if (hodnota >= 1) {
-//         soucet = soucet + hodnota;
-//     }
-// });
-// document.body.innerHTML += `${soucet}`;
+const h12 = document.querySelector('.uloha12');
+let soucet3 = 0;
+numbers.forEach((hodnota) => {
+    if (hodnota >= 1) {
+        soucet3 = soucet3 + hodnota;
+    }
+});
+h12.innerHTML += ` ${soucet3}`;
